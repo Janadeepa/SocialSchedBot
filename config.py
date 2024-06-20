2a71c8fdefd4ea534e59bfb609a465ed40cb2f38
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2024-06-20 11:14:25 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+TWITTER_API_KEY = 'your_twitter_api_key'
+TWITTER_API_SECRET = 'your_twitter_api_secret'
+TWITTER_ACCESS_TOKEN = 'your_twitter_access_token'
+TWITTER_ACCESS_SECRET = 'your_twitter_access_secret'
+
+INSTAGRAM_USERNAME = 'your_instagram_username'
+INSTAGRAM_PASSWORD = 'your_instagram_password'
